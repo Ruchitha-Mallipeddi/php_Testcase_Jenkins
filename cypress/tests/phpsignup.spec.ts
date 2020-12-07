dad45a2d8e456dd412c66cc1c516042059f92806
Gul <57442171+Gul224@users.noreply.github.com>
2020-12-07 12:49:53 +0530

    Update phpsignup.spec.ts

--- cypress/tests/phpsignup.spec.ts
+++ cypress/tests/phpsignup.spec.ts
@@ -140,3 +140,12 @@ it("All Blank field", function(){
   .click()
   cy.get('.signupbtn').click
 })
+// likely want to do this in a support file
+// so it's applied to all spec files
+// cypress/support/index.js
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
