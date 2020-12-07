083b6d315d3d58273be35b05eee7bad1289e4b87
Gul <57442171+Gul224@users.noreply.github.com>
2020-12-07 12:49:33 +0530

    Update phplogin.spec.ts

--- cypress/tests/phplogin.spec.ts
+++ cypress/tests/phplogin.spec.ts
@@ -78,3 +78,12 @@ it ("invalid Username", function(){
 //      .click
 //      cy.get('#loginfrm > .btn-primary').click
 })
+// likely want to do this in a support file
+// so it's applied to all spec files
+// cypress/support/index.js
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
