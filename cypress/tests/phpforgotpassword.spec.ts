90c64c2d31bd714c093ff35c532b5172729a3a94
Gul <57442171+Gul224@users.noreply.github.com>
2020-12-07 12:45:40 +0530

    Update phpforgotpassword.spec.ts

--- cypress/tests/phpforgotpassword.spec.ts
+++ cypress/tests/phpforgotpassword.spec.ts
@@ -38,5 +38,13 @@ it("Blank reset email", function(){
     .should("have.text","Email Not Found")
 })
 
+// likely want to do this in a support file
+// so it's applied to all spec files
+// cypress/support/index.js
 
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false
+  })
 
